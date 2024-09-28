3ad6c6dfb0c5ae516205cdba3bdf629327d9e9dc
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-09-28 13:49:02 +0100

    ouverture des oringines du CORS

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(cors());
 // app.use(
 //   cors({
 //     origin: ["frontend.fizitech.org"],
