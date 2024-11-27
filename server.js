227ee0bce5a0e35b5a3d81f4edcd68179f07a56b
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-11-27 14:51:40 +0100

    le backend

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["https://lion-print.net"],
+    origin: ["https://www.lion-print.net"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
